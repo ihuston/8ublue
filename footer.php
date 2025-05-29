8d14a9c1e707c99d2c58cf1670354f9415784d61
Ian Huston <ian@ianhuston.net>
2025-05-29 22:57:03 +0100

    Add privacy policy

--- footer.php
+++ footer.php
@@ -13,7 +13,7 @@
 	</div><!-- #main .wrapper -->
 	<footer id="colophon" role="contentinfo">
         <div class="site-info">
-            All content &copy; 8&uacute; Calafort Gas&oacute;ga Mara. Maintained by <a href="http://www.ianhuston.net">Ian Huston</a> - <a href="mailto:webmaster@8ucalafort.org">webmaster@8ucalafort.org</a>.
+            All content &copy; 8&uacute; Calafort Gas&oacute;ga Mara. Maintained by <a href="http://www.ianhuston.net">Ian Huston</a> - <a href="mailto:webmaster@8ucalafort.org">webmaster@8ucalafort.org</a> - <a href="https://www.8ucalafort.org/privacy-policy/">Privacy Policy</a> -
 			<?php do_action( 'twentytwelve_credits' ); ?>
 			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
 		</div><!-- .site-info -->
